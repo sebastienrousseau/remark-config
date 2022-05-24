dd375080da291c18b1489802b38fc2a628daca49
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2022-05-24 14:04:28 +0100

    v0.0.3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   "plugins": [
     "remark-preset-lint-recommended",
     "remark-preset-lint-markdown-style-guide",
-    "remark-preset-lint-consistent"
+    "remark-preset-lint-consistent",
+    "remark-footnotes"
   ]
 };
